b3cac8305a40f873fb18f8ddee8bbb1cbb735386
lara <lara@192larapc.168.1.3>
2013-11-20 17:07:38 +0200

    else

---
+++ 4.py
@@ -0,0 +1,12 @@
+a = raw_input('hahahahaha')
+if a == "1":
+    fanction1_1()
+    fanction1_2()
+elif a == "2":
+    pass
+elif a == "3":
+    pass
+elif a == "4":
+    pass
+else:
+    print "Chois hahahaha"
---
+++ 5.py
@@ -0,0 +1,6 @@
+x = 2
+if x < 1:
+    print "You win"
+else:
+    if x < 10:
+        print "hahahaha"
